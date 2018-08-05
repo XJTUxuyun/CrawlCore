00e25f40531e83afcbeeaf99801fa97f15ad5b12
wyl <xuyun_xjtu@qq.com>
2018-08-05 08:29:14 +0800

    Initial Commit

---
+++ CourtCrawlCore/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CourtCrawlCore
+//
+//  Created by 王媛莉 on 2018/8/5.
+//  Copyright © 2018 徐云. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
